89ef194f33d0af92cdb35133d95e7bb56a6d96ff
motida <motida2@gmail.com>
2019-01-22 22:41:07 +0200

    refactoring plus removing locust and implementing using threads

--- demo-app/README.md
+++ demo-app/README.md
@@ -1,14 +1,13 @@
 #Demo App
-Running simulation to insert rows into cassandra:
+Running simulation to insert rows into Cassandra and Bigtable:
 
-1) Make sure locust is installed in your environment.
-2) Run simlulation process using command:
-   <br><i>locust -f src/simulation.py</i>
-3) Open browser at: http://127.0.0.1:8089/
-4) Start simulation by choosing 
-   <br>Number of users to simulate = number of servers to simulate. 
-   <br> Hatch rate = 0 to spawn all
-  'servers' imediately.
-   <br> and press <i>start</i> 
-5) Test data is being inserted by using the following sql from cqlsh
+**python simulation.py --servers=5 --cassandra_host=<host ip> 
+  --cassandra_db=\<cass db\> 
+  --bt_project_id=\<google project id\> 
+  --bt_instance_id=\<bigtable instance id\> 
+  --bt_table=\<table name\> 
+  --bt_column_family=\<column family name\>**
+- Test data is being inserted to cassandra by using the following sql from cqlsh
     <br><i>select count(*) from case_ks.metrics;</i>
+- Test data is being inserted to Bigtable by using the following cbt command
+    <br><i>cbt count metrics</i>
--- demo-app/simulation.py
+++ demo-app/simulation.py
@@ -1,20 +1,79 @@
-from locust import Locust, TaskSet, task
+from dl import metric as metric_api
 
-from dl import metric
+from cassandra.cluster import Cluster
+from google.cloud import bigtable
 
-
-class ServerBehavior(TaskSet):
-
-    @task
-    def sample(self):
-        row = metric.create_metric(id(self))
-#        metric.insert_row_cassandra(row)
-        metric.insert_row_bigtable(row)
-        print('----->id= {}'.format(id(self)))
+import argparse
+import datetime
+import ipaddress
+import random
+import threading
+import time
 
 
-class Server(Locust):
-    task_set = ServerBehavior
-    min_wait = 5000
-    max_wait = 9000
+def emit_metrics(server, cass_session, bt_table, column_family_id):
+    count = 0
+    #while True:
+    for _ in range(1, 2):
+        count += 1
+        metric = metric_api.create_metric(server)
+        metric_api.insert_row_cassandra(metric, cass_session)
+        metric_api.insert_row_bigtable(metric, bt_table, column_family_id)
+        print('Server {} emitted total {} metric'.format(server, count))
+        time.sleep(random.randrange(5, 9))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument(
+        '--servers',
+        type=int,
+        help='Number of servers to simulate',
+        default=1)
+    parser.add_argument(
+        '--cassandra_host',
+        help='Cassandra endpoint',
+        required=True)
+    parser.add_argument(
+        '--cassandra_db',
+        help='Cassandra keyspace',
+        required=True)
+    parser.add_argument(
+        '--bt_project_id',
+        help='Bigtable project id',
+        required=True)
+    parser.add_argument(
+        '--bt_instance_id',
+        help='Bigtable instance id',
+        required=True)
+    parser.add_argument(
+        '--bt_table',
+        help='Bigtable table name',
+        required=True)
+    parser.add_argument(
+        '--bt_column_family',
+        help='Bigtable column family',
+        required=True)
+
+    args = parser.parse_args()
+
+    # Cassandra connection
+    cass_cluster = Cluster([args.cassandra_host])
+    cass_session = cass_cluster.connect(args.cassandra_db)
+    # Bigtable connection
+    bt_client = bigtable.Client(project=args.bt_project_id, admin=True)
+    bt_instance = bt_client.instance(args.bt_instance_id)
+    bt_table = bt_instance.table(args.bt_table)
+    column_family_id = args.bt_column_family
+
+    num_of_servers = args.servers
+
+    servers = [random.randrange(0, 2**32) for _ in range(num_of_servers)]
+    print('=bringing up servers {}'.format(servers))
+    for server in servers:
+        thread = threading.Thread(target=emit_metrics, args=(server, cass_session, bt_table, column_family_id))
+        thread.start()
 
